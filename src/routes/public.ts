8d20917b2214ba2a3b253087777d3f1a95f66904
Tubebox <tubeboxhelp@gmail.com>
2026-05-01 16:45:21 +0530

    Update public.ts

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { getVideos, getVideoById, viewVideo, likeVideo, downloadVideo } from '../controllers/publicController';
+import { getVideos, getVideoById, viewVideo, likeVideo, downloadVideo ,   getVideosByIds } from '../controllers/publicController';
 
 const router = Router();
 
 // Publicly accessible routes
 router.get('/videos', getVideos);
 router.get('/videos/:id', getVideoById);
+router.post('/videos/bulk', getVideosByIds);
+
+
 router.post('/videos/:id/view', viewVideo);
 router.post('/videos/:id/like', likeVideo);
 router.post('/videos/:id/download', downloadVideo);
